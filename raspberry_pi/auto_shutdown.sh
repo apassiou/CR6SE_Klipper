0707564dbecc6cb4b00b4c7a38844314c58e04f0
apassiou <killwarez@yahoo.com>
2023-05-31 23:32:10 -0700

    added check for ttyUSB0 to ttyUSB3

--- raspberry_pi/auto_shutdown.sh
+++ raspberry_pi/auto_shutdown.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-#v05.2023 by Duxa
+#v06.2023 by Duxa
 
 #Put this script into root crontab
 #sudo crontab -e
 #@reboot /home/pi/auto_shutdown.sh <-- change directory as appropriate
-#NOTE: This simply checks if /dev/ttyUSB0 is present, if printer is powered off it wont be. If thats the case raspberry pi shuts down.
+#NOTE: This simply checks if /dev/ttyUSB[0-3] are present, if printer is powered off it wont be. If thats the case raspberry pi shuts down.
 #NOTE: Checks are done every 15 minutes (after initial 15 minute timeout). Adjust below as desired.
 #NOTE: If your pi is connected to CR6SE via another means, this will need to be modified to work
 
 sleep 15m
 while true
 do
-  if [ -e /dev/ttyUSB0 ]; then
+  if [ -e /dev/ttyUSB0 ] || [ -e /dev/ttyUSB1 ] || [ -e /dev/ttyUSB2 ] || [ -e /dev/ttyUSB3 ]; then
     printf "Printer is On\n"
     sleep 15m
   else
